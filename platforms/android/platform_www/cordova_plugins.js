b242688b339d7928651b7a91e2959d88eb1a93ef
blackarez <angellg1990@gmail.com>
2018-01-22 13:09:04 -0500

    module plugins and plataforms

--- platforms/android/platform_www/cordova_plugins.js
+++ platforms/android/platform_www/cordova_plugins.js
@@ -1,6 +1,14 @@
 cordova.define('cordova/plugin_list', function(require, exports, module) {
 module.exports = [
   {
+    "id": "cordova-plugin-nativeaudio.nativeaudio",
+    "file": "plugins/cordova-plugin-nativeaudio/www/nativeaudio.js",
+    "pluginId": "cordova-plugin-nativeaudio",
+    "clobbers": [
+      "window.plugins.NativeAudio"
+    ]
+  },
+  {
     "id": "cordova-plugin-camera.Camera",
     "file": "plugins/cordova-plugin-camera/www/CameraConstants.js",
     "pluginId": "cordova-plugin-camera",
@@ -33,23 +41,6 @@ module.exports = [
     ]
   },
   {
-    "id": "cordova-plugin-device.device",
-    "file": "plugins/cordova-plugin-device/www/device.js",
-    "pluginId": "cordova-plugin-device",
-    "clobbers": [
-      "device"
-    ]
-  },
-  {
-    "id": "cordova-plugin-email-composer.EmailComposer",
-    "file": "plugins/cordova-plugin-email-composer/www/email_composer.js",
-    "pluginId": "cordova-plugin-email-composer",
-    "clobbers": [
-      "cordova.plugins.email",
-      "plugin.email"
-    ]
-  },
-  {
     "id": "cordova-plugin-geolocation.geolocation",
     "file": "plugins/cordova-plugin-geolocation/www/android/geolocation.js",
     "pluginId": "cordova-plugin-geolocation",
@@ -64,88 +55,11 @@ module.exports = [
     "runs": true
   },
   {
-    "id": "cordova-plugin-nativeaudio.nativeaudio",
-    "file": "plugins/cordova-plugin-nativeaudio/www/nativeaudio.js",
-    "pluginId": "cordova-plugin-nativeaudio",
-    "clobbers": [
-      "window.plugins.NativeAudio"
-    ]
-  },
-  {
     "id": "cordova-plugin-nativegeocoder.NativeGeocoder",
     "file": "plugins/cordova-plugin-nativegeocoder/www/NativeGeocoder.js",
     "pluginId": "cordova-plugin-nativegeocoder",
     "clobbers": [
       "nativegeocoder"
-    ]
-  },
-  {
-    "id": "cordova-plugin-nativestorage.mainHandle",
-    "file": "plugins/cordova-plugin-nativestorage/www/mainHandle.js",
-    "pluginId": "cordova-plugin-nativestorage",
-    "clobbers": [
-      "NativeStorage"
-    ]
-  },
-  {
-    "id": "cordova-plugin-nativestorage.LocalStorageHandle",
-    "file": "plugins/cordova-plugin-nativestorage/www/LocalStorageHandle.js",
-    "pluginId": "cordova-plugin-nativestorage"
-  },
-  {
-    "id": "cordova-plugin-nativestorage.NativeStorageError",
-    "file": "plugins/cordova-plugin-nativestorage/www/NativeStorageError.js",
-    "pluginId": "cordova-plugin-nativestorage"
-  },
-  {
-    "id": "cordova-plugin-splashscreen.SplashScreen",
-    "file": "plugins/cordova-plugin-splashscreen/www/splashscreen.js",
-    "pluginId": "cordova-plugin-splashscreen",
-    "clobbers": [
-      "navigator.splashscreen"
-    ]
-  },
-  {
-    "id": "cordova-plugin-statusbar.statusbar",
-    "file": "plugins/cordova-plugin-statusbar/www/statusbar.js",
-    "pluginId": "cordova-plugin-statusbar",
-    "clobbers": [
-      "window.StatusBar"
-    ]
-  },
-  {
-    "id": "cordova-sms-plugin.Sms",
-    "file": "plugins/cordova-sms-plugin/www/sms.js",
-    "pluginId": "cordova-sms-plugin",
-    "clobbers": [
-      "window.sms"
-    ]
-  },
-  {
-    "id": "de.appplant.cordova.plugin.local-notification.LocalNotification",
-    "file": "plugins/de.appplant.cordova.plugin.local-notification/www/local-notification.js",
-    "pluginId": "de.appplant.cordova.plugin.local-notification",
-    "clobbers": [
-      "cordova.plugins.notification.local",
-      "plugin.notification.local"
-    ]
-  },
-  {
-    "id": "de.appplant.cordova.plugin.local-notification.LocalNotification.Core",
-    "file": "plugins/de.appplant.cordova.plugin.local-notification/www/local-notification-core.js",
-    "pluginId": "de.appplant.cordova.plugin.local-notification",
-    "clobbers": [
-      "cordova.plugins.notification.local.core",
-      "plugin.notification.local.core"
-    ]
-  },
-  {
-    "id": "de.appplant.cordova.plugin.local-notification.LocalNotification.Util",
-    "file": "plugins/de.appplant.cordova.plugin.local-notification/www/local-notification-util.js",
-    "pluginId": "de.appplant.cordova.plugin.local-notification",
-    "merges": [
-      "cordova.plugins.notification.local.core",
-      "plugin.notification.local.core"
     ]
   },
   {
@@ -158,11 +72,19 @@ module.exports = [
     "runs": true
   },
   {
-    "id": "cordova-plugin-badge.Badge",
-    "file": "plugins/cordova-plugin-badge/www/badge.js",
-    "pluginId": "cordova-plugin-badge",
+    "id": "cordova-plugin-device.device",
+    "file": "plugins/cordova-plugin-device/www/device.js",
+    "pluginId": "cordova-plugin-device",
     "clobbers": [
-      "cordova.plugins.notification.badge"
+      "device"
+    ]
+  },
+  {
+    "id": "cordova-plugin-splashscreen.SplashScreen",
+    "file": "plugins/cordova-plugin-splashscreen/www/splashscreen.js",
+    "pluginId": "cordova-plugin-splashscreen",
+    "clobbers": [
+      "navigator.splashscreen"
     ]
   },
   {
@@ -172,30 +94,57 @@ module.exports = [
     "clobbers": [
       "facebookConnectPlugin"
     ]
+  },
+  {
+    "id": "cordova-plugin-badge.Badge",
+    "file": "plugins/cordova-plugin-badge/www/badge.js",
+    "pluginId": "cordova-plugin-badge",
+    "clobbers": [
+      "cordova.plugins.notification.badge"
+    ]
+  },
+  {
+    "id": "cordova-plugin-local-notification.LocalNotification",
+    "file": "plugins/cordova-plugin-local-notification/www/local-notification.js",
+    "pluginId": "cordova-plugin-local-notification",
+    "clobbers": [
+      "cordova.plugins.notification.local"
+    ]
+  },
+  {
+    "id": "cordova-plugin-local-notification.LocalNotification.Core",
+    "file": "plugins/cordova-plugin-local-notification/www/local-notification-core.js",
+    "pluginId": "cordova-plugin-local-notification",
+    "clobbers": [
+      "cordova.plugins.notification.local.core",
+      "plugin.notification.local.core"
+    ]
+  },
+  {
+    "id": "cordova-plugin-local-notification.LocalNotification.Util",
+    "file": "plugins/cordova-plugin-local-notification/www/local-notification-util.js",
+    "pluginId": "cordova-plugin-local-notification",
+    "merges": [
+      "cordova.plugins.notification.local.core",
+      "plugin.notification.local.core"
+    ]
   }
 ];
 module.exports.metadata = 
 // TOP OF METADATA
 {
-  "cordova-plugin-add-swift-support": "1.7.0",
-  "cordova-plugin-app-event": "1.2.1",
-  "cordova-plugin-camera": "2.4.1",
-  "cordova-plugin-console": "1.0.5",
-  "cordova-plugin-device": "1.1.4",
-  "cordova-plugin-email-composer": "0.8.7",
-  "cordova-plugin-geolocation": "2.4.3",
-  "cordova-plugin-ionic-webview": "1.1.16",
   "cordova-plugin-nativeaudio": "3.0.9",
+  "cordova-plugin-camera": "2.4.1",
+  "cordova-plugin-geolocation": "2.4.3",
   "cordova-plugin-nativegeocoder": "2.0.4",
-  "cordova-plugin-nativestorage": "2.2.2",
-  "cordova-plugin-splashscreen": "4.0.3",
-  "cordova-plugin-statusbar": "2.2.2",
-  "cordova-plugin-whitelist": "1.3.1",
-  "cordova-sms-plugin": "0.1.11",
-  "de.appplant.cordova.plugin.local-notification": "0.8.5",
   "ionic-plugin-keyboard": "2.2.1",
-  "cordova-plugin-badge": "0.8.5",
-  "cordova-plugin-facebook4": "1.9.1"
+  "cordova-plugin-whitelist": "1.3.3",
+  "cordova-plugin-device": "1.1.7",
+  "cordova-plugin-splashscreen": "4.1.0",
+  "cordova-plugin-ionic-webview": "1.1.16",
+  "cordova-plugin-facebook4": "1.9.1",
+  "cordova-plugin-badge": "0.8.6",
+  "cordova-plugin-local-notification": "0.9.0-beta.2"
 };
 // BOTTOM OF METADATA
 });
